70e94ff90bdd50b2ab211b87d0d6ce7d70fd888e
Chang1l <81501866+Chang1l@users.noreply.github.com>
2021-09-15 13:36:46 +0900

    Create find_life

--- src/pages/contents/results.js
+++ src/pages/contents/results.js
@@ -3,8 +3,8 @@ const results = [
     //0
     type: "HIDC",
     color: "#95D4E2",
-    title: "그 누구도 나의 잠을<br/>방해하지는 못해요",
-    name: "잠만보",
+    title: "ㅇㅇ",
+    name: "{$name}",
     img: "../result-image/reaper.png",
     description: "당신을 응원할게요 :)",
     prob: 3,
